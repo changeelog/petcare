bbc0c2b80d357f6278e461897e4e65b831581f8f
Eugene <webeugene@proton.me>
2024-10-18 03:12:56 +0300

    Removed comments from main file for backend

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -13,17 +13,13 @@ import (
 )
 
 func gracefulShutdown(apiServer *http.Server) {
-	// Create context that listens for the interrupt signal from the OS.
 	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
 
-	// Listen for the interrupt signal.
 	<-ctx.Done()
 
 	log.Println("shutting down gracefully, press Ctrl+C again to force")
 
-	// The context is used to inform the server it has 5 seconds to finish
-	// the request it is currently handling
 	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
 	defer cancel()
 	if err := apiServer.Shutdown(ctx); err != nil {
